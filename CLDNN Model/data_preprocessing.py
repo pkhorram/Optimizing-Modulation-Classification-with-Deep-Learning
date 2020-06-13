8aa90eab57a3db0cc00db6d9494f7f9bb3691be2
ankush18194 <52859260+ankush18194@users.noreply.github.com>
2020-06-12 19:58:05 -0700

    Updating Preprocessing  to include bigger dataset
    Have commented on the part that needs to be changed for bigger dataset

--- CLDNN Model/data_preprocessing.py
+++ CLDNN Model/data_preprocessing.py
@@ -1,6 +1,7 @@
 import pickle
 from utils import *
 
+# Open this ./RML2016.10b/RML2016.10b.dat if you want to preprocess bigger dataset 
 with open("RML2016.10a_dict.pkl", "rb") as p:
     d = pickle.load(p, encoding='latin1')
 
@@ -24,6 +25,6 @@ for keys in d.keys():
         # convert labels to one-hot encoders.
         SNRs[keys[1]].append([onehot_encoder(label_dict, keys[0]),np.array(arrays)]) 
 
-outfile = open('dataset','wb')
+outfile = open('dataset','wb') # Save with other file name when using bigger data set (dataset_big)
 pickle.dump(SNRs, outfile)
 outfile.close()
